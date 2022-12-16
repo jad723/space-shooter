81d8aa4100872e1dd65365af9534ba6a4761ffb1
jad723 <jadmaalouf2003.jm15@gmail.com>
2022-12-16 12:21:59 +0200

    Added player controls

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,12 +24,14 @@ namespace Space_Shooter
     {
 
         DispatcherTimer gameTimer = new DispatcherTimer();
+
         bool moveLeft, moveRight;
+
         List<Rectangle> itemRemover = new List<Rectangle>();
 
         Random rand = new Random();
 
-        int enemySpriteCounter = 0, enemyCounter = 100, playerSpeed = 10, limit = 50, score = 0, damaged = 0, enemySpeed = 10;
+        int enemySpriteCounter = 0, enemyCounter = 100, playerSpeed = 10, limit = 50, score = 0, damage = 0, enemySpeed = 10;
 
         Rect playerHitBox;
 
@@ -58,7 +60,22 @@ namespace Space_Shooter
 
         private void GameLoop(object sender, EventArgs e)
         {
-            
+            playerHitBox = new Rect(Canvas.GetLeft(player), Canvas.GetTop(player), player.Width, player.Height);
+
+            enemyCounter -= 1;
+
+            scoreText.Content = "Score: " + score;
+            damageText.Content = "Damage " + damage;
+
+            if(enemyCounter < 0)
+            {
+                MakeEnemies();
+                enemyCounter = limit;
+            }
+
+            if(moveLeft == true && Canvas.GetLeft(player) > 10) Canvas.SetLeft(player, Canvas.GetLeft(player) - playerSpeed);
+
+            if(moveRight == true && Canvas.GetLeft(player) + 90 < Application.Current.MainWindow.Width) Canvas.SetLeft(player, Canvas.GetLeft(player) + playerSpeed);
         }
 
         private void OnKeyDown(object sender, KeyEventArgs e)
@@ -95,7 +112,44 @@ namespace Space_Shooter
 
         private void MakeEnemies()
         {
+            ImageBrush enemySprite = new ImageBrush();
 
+            enemySpriteCounter = rand.Next(1, 5);
+
+            switch (enemySpriteCounter)
+            {
+                case 1:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/1.png"));
+                    break;
+
+                case 2:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/2.png"));
+                    break;
+
+                case 3:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/3.png"));
+                    break;
+
+                case 4:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/4.png"));
+                    break;
+
+                case 5:
+                    enemySprite.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/5.png"));
+                    break;
+            }
+
+            Rectangle newEnemy = new Rectangle
+            {
+                Tag = "enemy",
+                Height = 50,
+                Width = 56,
+                Fill = enemySprite
+            };
+
+            Canvas.SetTop(newEnemy, -100);
+            Canvas.SetLeft(newEnemy, rand.Next(30, 430));
+            MyCanvas.Children.Add(newEnemy);
         }
     }
 }
