ae7834043ae510f4d7fead0018fedb4d82a00d46
jad723 <jadmaalouf2003.jm15@gmail.com>
2022-12-16 11:17:20 +0200

    Adde game timer and background

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Space_Shooter
 {
@@ -20,9 +21,55 @@ namespace Space_Shooter
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        bool moveLeft, moveRight;
+        List<Rectangle> itemRemover = new List<Rectangle>();
+
+        Random rand = new Random();
+
+        int enemySpriteCounter = 0, enemyCounter = 100, playerSpeed = 10, limit = 50, score = 0, damaged = 0, enemySpeed = 10;
+
+        Rect playerHitBox;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick += GameLoop;
+            gameTimer.Start();
+
+            MyCanvas.Focus();
+
+            ImageBrush bg = new ImageBrush();
+
+            bg.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/purple.png"));
+            bg.TileMode = TileMode.Tile;
+            bg.Viewport = new Rect(0, 0, 0.15, 0.15);
+            bg.ViewportUnits = BrushMappingMode.RelativeToBoundingBox;
+            MyCanvas.Background = bg;
+
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void MakeEnemies()
+        {
+
         }
     }
 }
